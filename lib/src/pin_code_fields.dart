49bca3022b67f7db2774f5045a1aa76f18e51dd2
bdingInc <75005681+bdingInc@users.noreply.github.com>
2021-01-15 16:31:06 +0900

    widget.enabled == false 시 disabledColor 사용 안함

--- lib/src/pin_code_fields.dart
+++ lib/src/pin_code_fields.dart
@@ -433,9 +433,9 @@ class _PinCodeTextFieldState extends State<PinCodeTextField>
 
   // selects the right color for the field
   Color _getColorFromIndex(int index) {
-    if (!widget.enabled) {
-      return _pinTheme.disabledColor;
-    }
+//     if (!widget.enabled) {
+//       return _pinTheme.disabledColor;
+//     }
     if (((_selectedIndex == index) ||
             (_selectedIndex == index + 1 && index + 1 == widget.length)) &&
         _focusNode.hasFocus) {
